ae8cfa90de9fcd2f7aa5c6fe678cf1296449614c
Wubbo Boiten <97003968@st.deltion.nl>
2026-04-07 14:31:24 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Wubbo Boiten on 23/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var numberFormatter: NumberFormatter = {
+        var numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
+    }()
+    
+    @State private var checkAmount: Double = 0
+    @State private var numberOfPeople: Int = 0
+    @State private var tipPercentage: Int = 0
+    
+    
+    var totalPerPerson: Double   {
+        
+        
+        
+        
+       // checkAmount.rounded(.down) / Double(numberOfPeople)
+    }
+    
+    
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section(header: Text("Total Amount")){
+                    TextField("\(checkAmount)", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "eur"))
+                        .keyboardType(.decimalPad)
+                }
+                Section(header: Text("Amount of people")){
+                    
+                    TextField("Number of people:", value: $numberOfPeople, formatter: numberFormatter)
+                        .keyboardType(.decimalPad)
+                }
+                Section(header: Text("Percentage of tip")){
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text("\($0)%")
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                }
+                Section(header: Text("To Pay")){
+                    Text("€\(totalPerPerson)")
+                }
+            }
+            .navigationTitle("WeSplit")
+            
+                }
+            }
+        }
+        
+    
+
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Wubbo Boiten on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+ 
